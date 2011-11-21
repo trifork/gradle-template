db7b4f2edf264f867b2aff5616dcadf8ae0d853e
Thomas Børlum <thomas@borlum.dk>
2011-11-21 22:15:07 +0100

    Add a few common libs and bump versions.

--- config/libraries.groovy
+++ config/libraries.groovy
@@ -1,24 +1,25 @@
-//////////////////////////////////////////////////////////////////////////////
-// This file contains a handy list of dependencies' names and versions.
-// This can be used to make sure dependencies are consistently versioned 
-// in your projects.
-//////////////////////////////////////////////////////////////////////////////
+// A single place to keep all information about dependency versions.
+// This allows you to keep versions consistent across sub-projects.
 
-slf4jVersion = '1.6.1'
+def SLF4J_VERSION = '1.6.4'
 
 libraries = [
 
-	// Javassist
-
-	javassist: 'javassist:javassist:3.12.0.GA',
-
 	// Logging
 
-	slf4j_api: 'org.slf4j:slf4j-api:' + slf4jVersion,
-	jcl_slf4j: 'org.slf4j:jcl-over-slf4j:' + slf4jVersion,
-	log4j_slf4j: 'org.slf4j:log4j-over-slf4j:' + slf4jVersion,
+	slf4j_api: 'org.slf4j:slf4j-api:' + SLF4J_VERSION,
+	jcl_slf4j: 'org.slf4j:jcl-over-slf4j:' + SLF4J_VERSION,
+	log4j_slf4j: 'org.slf4j:log4j-over-slf4j:' + SLF4J_VERSION,
+	
+	logback: 'ch.qos.logback:logback-classic:1.0.0',
 
 	// Testing
 
-	testng: 'org.testng:testng:5.14.6'
+	junit: 'junit:junit:4.10',
+	mockito: 'org.mockito:mockito-core:1.9.0-rc1',
+	
+	// Commons
+	
+	guava: 'com.google.guava:guava:10.0.1',
+    commons_lang: 'commons-lang:commons-lang:2.6',
 ]
